5a875ba0f177c1d784b983198b63f35ae7099555
Gwennael Gate <gwennael.gate@angus.ai>
2017-05-03 23:02:18 +0200

    Adding 1 character + command 3 leds

--- coeur_1/actions.ino
+++ coeur_1/actions.ino
@@ -4,13 +4,33 @@ void actions(int me, unsigned long received, Per l[]){
   }
   
   if(l[me].ir_expected_1 == received){
+    set_led_youpi();
     Serial.println("youpi youpi");
   }
   else if(l[me].ir_expected_2 == received){
+    set_led_moyen();
     Serial.println("peut mieux faire");
   }
   else{
+    set_led_non();
     Serial.println("pas content");
   }
 }
 
+void set_led_youpi(){
+  digitalWrite(PIN_LED_YOUPI, HIGH);
+  digitalWrite(PIN_LED_MOYEN, HIGH);
+  digitalWrite(PIN_LED_NON, HIGH);
+}
+
+void set_led_moyen(){
+  digitalWrite(PIN_LED_YOUPI, LOW);
+  digitalWrite(PIN_LED_MOYEN, HIGH);
+  digitalWrite(PIN_LED_NON, HIGH);
+}
+
+void set_led_non(){
+  digitalWrite(PIN_LED_YOUPI, LOW);
+  digitalWrite(PIN_LED_MOYEN, LOW);
+  digitalWrite(PIN_LED_NON, HIGH);
+}
--- coeur_1/coeur_1.ino
+++ coeur_1/coeur_1.ino
@@ -4,23 +4,31 @@
 #include <IRremote.h>
 #define RECV_PIN 8 // il faut vérifier que cette PIN permet la reception IR sur toutes les boards
 
+/// la je définis les PIN digital qui vont commander les 3 leds sur les arduinos...
+#define PIN_LED_YOUPI 9
+#define PIN_LED_MOYEN 10
+#define PIN_LED_NON 11
+
 struct Per { //un personnage c'est:
   char name[10]; //un nom
   unsigned int config_pin; //une unique pin arduino qui est branchée sur 5V, 
   // en changeant la PIN branchée sur 5V on fait changer de personnage un arduino en particulier
   unsigned long ir_send; //un code envoyé a tout le monde
-  unsigned long ir_expected_1; //un code expected pour etre super content
-  unsigned long ir_expected_2; //un code expected pour etre moyen content
+  unsigned long ir_expected_1; //un code expected pour etre youpi youpi
+  unsigned long ir_expected_2; //un code expected pour etre moyen content  
 };
 
 //Creation des personnage et des regles du scenario
+// les trucs entre les accolades, ce sont les valeurs que je décide de donner au 5 parametres de la structure Per (voir 5 lignes plus haut).
+// donc le "5" du personnage "reine" ci-dessous correspond à sa "config_pin", le "Ox93" correspond à son ir_send, etc...
 Per reine = {"reine", 5, 0x93, 0x92, 0x91}; //on crée le personnage "reine"
 Per fou = {"fou", 6, 0x91, 0x93, 0x92}; //on crée le personnage "fou"
-// Ici le scenario dit : le fou attent que la reine lui parle dans les oreilles, la reine elle veut qu'un troisieme
+Per roi = {"roi", 7, 0x92, 0x91, 0x93}; //on crée le personnage "roi"
+// Ici le scenario dit : roi -> reine -> fou -> roi pour que tout le monde soit "youpi youpi"
 // personnage non defini ici lui parle dans les oreilles
 
 
-Per l_pers[] = {reine, fou}; // on crée une liste avec tous les personnage utile pour la suite
+Per l_pers[] = {reine, fou, roi}; // on crée une liste avec tous les personnage utile pour la suite
 
 // Initialisations
 IRsend irsend;
@@ -36,6 +44,10 @@ void setup() {
   Serial.println(me);
   to_send = get_to_send(me, l_pers); //renvoie le code IR a envoyer
   Serial.println(to_send, HEX);
+  /// Préparation des PIN pour la commande des LEDS
+  pinMode(PIN_LED_YOUPI, OUTPUT);
+  pinMode(PIN_LED_MOYEN, OUTPUT);
+  pinMode(PIN_LED_NON, OUTPUT);
 }
 
 void loop() {
